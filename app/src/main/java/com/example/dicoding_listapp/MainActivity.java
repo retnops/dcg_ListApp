e4d0d7044878fb6b4cb56243531fff9534a75dd7
Unknown <retnoperwita@gmail.com>
2019-03-25 17:43:27 +0700

    2nd commit

--- app/src/main/java/com/example/dicoding_listapp/MainActivity.java
+++ app/src/main/java/com/example/dicoding_listapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.dicoding_listapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+private String [] dataName;
 
-    @Override
+
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    ListView listView = findViewById(R.id.lv_list);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+    listView.setAdapter(adapter);
     }
 }
