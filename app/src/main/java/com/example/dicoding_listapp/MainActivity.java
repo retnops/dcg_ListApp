cede0b0f42476b9109804ebe8677eeee5337fbb7
Unknown <retnoperwita@gmail.com>
2019-03-27 14:49:59 +0700

    3rd commit

--- app/src/main/java/com/example/dicoding_listapp/MainActivity.java
+++ app/src/main/java/com/example/dicoding_listapp/MainActivity.java
@@ -1,20 +1,65 @@
 package com.example.dicoding_listapp;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.os.Looper.prepare;
 
 public class MainActivity extends AppCompatActivity {
-private String [] dataName;
+private String[] dataName;
+private String[] dataDescription;
+private TypedArray dataPhoto;
+private HeroAdapter adapter;
+private ArrayList <Hero> heroes;
 
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new HeroAdapter(this);
+
     ListView listView = findViewById(R.id.lv_list);
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+//    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
     listView.setAdapter(adapter);
+
+    prepare();
+    addItem();
+
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener () {
+        @Override
+        public void onItemClick (AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(MainActivity.this, heroes.get(i).getName(), Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+
+    private void prepare() {
+    dataName = getResources().getStringArray(R.array.data_name);
+    dataDescription = getResources().getStringArray(R.array.data_description);
+    dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+    heroes = new ArrayList<>();
+
+    for (int i = 0; i < dataName.length; i++) {
+        Hero hero = new Hero ();
+        hero.setPhoto(dataPhoto.getResourceId(i, -1));
+        hero.setName(dataName[i]);
+        hero.setDescription(dataDescription[i]);
+        heroes.add(hero);
+    }
+
+    adapter.setHeroes(heroes);
     }
 }
